6014a0f1dce826b899bf001fc1e6e92e22ebaaf0
MShJSTM <gmodern2560@gmail.com>
2024-03-13 20:06:13 +0330

    BTC Turk ws

--- btcturk.py
+++ btcturk.py
@@ -13,14 +13,22 @@ def on_message(ws, message):
 
     decoded_message = json.loads(message)
     # pprint.pprint(decoded_message[1]["AO"])
-    pprint.pprint(decoded_message)
+    # pprint.pprint(decoded_message)
     # asks = decoded_message.get(decoded_message[1]["AO"])
     # bids = decoded_message.get("BO", [])
     # last_five_bids = bids[:5]
     # last_five_asks = asks[:5]
     # print("Last five buy orders", last_five_bids)
     # print("Last five sell orders", last_five_asks)
-
+    # pprint.pprint(decoded_message[1]["AO"])
+    asks = decoded_message[1]["AO"]
+    bids = decoded_message[1]["BO"]
+    
+    last_five_bids = bids[:5]
+    last_five_asks = asks[:5]
+    
+    print("Last five buy orders", last_five_bids)
+    print("Last five sell orders", last_five_asks)
 
 def on_error(ws, error):
     print(error)
@@ -46,3 +54,12 @@ if __name__ == "__main__":
         on_close=on_close)
     ws.on_open = on_open
     ws.run_forever()
+    
+    
+#export :    
+# Last five sell orders [
+#     {'A': '0.003584755872', 'P': '2365749'}
+#     , {'A': '0.00038912', 'P': '2365940'}
+#     , {'A': '0.02057192', 'P': '2366940'}
+#     , {'A': '0.01', 'P': '2366948'}
+#     , {'A': '0.01241117', 'P': '2366958'}]
